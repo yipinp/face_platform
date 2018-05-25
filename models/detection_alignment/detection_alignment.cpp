1cf2456b34acb21fe6ac82585abf11079dcfee9e
yipinp <wuqiseu_gmail@sina.com>
2018-05-25 23:08:18 +0800

    delete

---
+++ models/detection_alignment/detection_alignment.cpp
@@ -0,0 +1,22 @@
+#include "detection_alignment.h"
+
+
+
+
+detection_alignment::detection_alignment()
+{
+
+
+}
+
+detection_alignment::~detection_alignment()
+{
+
+
+}
+
+int main(int argc, char **argv)
+{
+
+    return 1;
+}
---
+++ models/detection_alignment/detection_alignment.h
@@ -0,0 +1,39 @@
+#ifndef _DETECTION_ALIGNMENT_H_
+#define _DETECTION_ALIGNMENT_H_
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/face.hpp>
+#include <opencv/cv.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing/render_face_detections.h>
+#include <dlib/image_processing.h>
+
+using namespace dlib;
+using namespace std;
+using namespace cv;
+
+class detection_alignment {
+
+public:
+    detection_alignment();
+    ~detection_alignment();
+    detection_alignment(std::queue<Mat> *data_source_queue_in,std::queue<Mat> *data_preprocess_queue_out);
+
+
+private:
+     bool dlib_face_detection_alignment();
+
+
+};
+
+
+
+#endif
